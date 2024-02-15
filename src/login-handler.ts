03a0a630e68c3d05565a3ea5e50eef614b2ba950
Timo Benkhard <tcbenkhard@gmail.com>
2024-02-15 17:03:19 +0100

    Add logging

--- src/login-handler.ts
+++ src/login-handler.ts
@@ -8,6 +8,8 @@ interface LoginRequest {
 
 const service = new PicnicService()
 export const handler = wrap_handler(async (event, context) => {
+    console.info(`Received request: ${event}`)
     const request_body = JSON.parse(event.body!)
+    console.info(`Parsed: ${request_body}`)
     return service.login(request_body.username, request_body.password)
 })
